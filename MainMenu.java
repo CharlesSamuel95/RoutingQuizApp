fdc24bca3296a3e2fcae7ad07faf46d8dfb393b0
Charles Samuel <csamuel1195@gmail.com>
2020-05-18 12:38:28 -0400

    Modifed Main Menu and Added a new Class

--- MainMenu.java
+++ MainMenu.java
@@ -62,6 +62,8 @@ public class MainMenu extends Application {
   public void start(Stage window) throws Exception {
     readAddressFile();
     createMainMenu(window);
+    //explainListView();
+    //explainRadioButton();
   }// end of start
 
   /**
@@ -146,7 +148,7 @@ public class MainMenu extends Application {
         GridPane menu = new GridPane();
 
       /**Main menu components */
-        Text introText = new Text("Welcome, Please chose your quiz options and hit the next page button\n");
+        Text introText = new Text("Welcome, Please choose your quiz options and hit the next page button\n");
         RadioButton sequentialBtn = new RadioButton("Sequential");
         RadioButton randomBtn = new RadioButton("Random");
 
@@ -193,12 +195,12 @@ public class MainMenu extends Application {
         listViewLabel.setFont(Font.font("Times", FontWeight.SEMI_BOLD,16));
         questionImg.setOnMouseEntered(e->{mainMenu.setCursor(Cursor.HAND);});
         questionImg.setOnMouseExited(e->{mainMenu.setCursor(Cursor.DEFAULT);});
-        questionImg.setOnMouseClicked(e->{mainMenuExplainListView();});
+        questionImg.setOnMouseClicked(e->{explainListView();});
 
         radioButtonLabel.setFont(Font.font("Times", FontWeight.SEMI_BOLD, 16));
         questionImg2.setOnMouseEntered(e->{mainMenu.setCursor(Cursor.HAND);});
         questionImg2.setOnMouseExited(e->{mainMenu.setCursor(Cursor.DEFAULT);});
-        questionImg2.setOnMouseClicked(e->{mainMenuExplainRadioButton();});
+        questionImg2.setOnMouseClicked(e->{explainRadioButton();});
 
         nextPageBtn.setOnMouseEntered(e ->{mainMenu.setCursor(Cursor.HAND);});
         nextPageBtn.setOnMouseExited(e->{mainMenu.setCursor(Cursor.DEFAULT);});
@@ -235,21 +237,106 @@ public class MainMenu extends Application {
 
    /**
     * Creates a window that explains the listview option.
+    * The user is able to select which address will appear in the quiz by
+    * selecting an element from this listview. For example if the user selects
+    * A then only addresses that start with A will be included; if A and B are 
+    * selected then only addresses that start with A abd B will be included, etc.
+    * By default all addresses will be included. Hold Ctrl to select multiple items
+    * in listview.
+    * 
+    * NOTE: This does not determine the number of questions a user will receive.
     */
-    private void mainMenuExplainListView(){
-      FlowPane pane = new FlowPane(new Text("new window"));
-      Scene scene = new Scene(pane,100,100);
+    private void explainListView(){
+      String text = "The items in this listview contains letters that can range from A to Z. " + 
+                    "The letters represents addresses that start with that letter.\n For example, " +
+                    "'A' represents all addresses that start with 'A', 'B' represents all addresses\n " +
+                    "that start with 'B', etc. Selecting an item will determine which addresses " +
+                    "will be included in the quiz.\n If 'A' is selected then all addresses that start " + 
+                    "with 'A' will be included, etc. You can select multiple items in the listview " + 
+                    "by holding Ctrl.\n If you only, for exapmle, choose 'A' and 'B' then the quiz will " + 
+                    "only include addresses that start with A and B.\n\n" + 
+                    "NOTE: This does not determine the number of questions you will receive.";
+      
+      Text textExplain = new Text(text);
+      textExplain.setFont(Font.font("Times", FontWeight.BOLD, 15));
+
+      FlowPane pane = new FlowPane(textExplain);
+      //pane.setAlignment(Pos.CENTER);
+
+      Scene scene = new Scene(pane);
       Stage window = new Stage();
-      window.setTitle("new window");
+      window.setTitle("Listview");
       window.setScene(scene);
       window.show();
     }
 
     /**
      * Creates a window that explains the radio button options.
+     * These buttons determine if the user will receive their questions
+     * sequentially (list ordering) or randomly. For example if the user choses
+     * to include only A address and selects sequential, then the first few address
+     * (i.e questions) will be 
+     * 
+     * ABBOTSBURY,LN,31088,R020
+     * ABERCORN,ST,31088,RO21
+     * ABNEY,CT,31028,C031
+     * ACORN RIDGE,CT,31088,C028
+     * ATERBURNER,WAY,31098,C050
+     * AFTON,CT,31028,C043
+     * AINSLEY,CT,31093,R004
+     * AIR PARK,DR,31088,R001.
+     * 
+     * If random is selected then address ordering(i.e question ordering) will be random.
+     * 
+     * NOTE: If more then one value in the listview is selected (say A and B) and sequential 
+     * is chosen, then the user will have to answear all A address questions before they will 
+     * encounter a B address question.
      */
-    private void mainMenuExplainRadioButton(){
+    private void explainRadioButton(){
+      String t1 = " These buttons determine if questions will be received " +
+      "sequentially (list ordering) or randomly.\n For example if you chose " + 
+      "to include only 'A' address and selects sequential,\n then the first few address " +
+      "(i.e questions) will be in this order\n";
 
+      String t2 = 
+         "ABBOTSBURY,LN,31088,R020\n" +
+         "ABERCORN,ST,31088,RO21\n" +
+         "ABNEY,CT,31028,C031\n" +
+         "ACORN RIDGE,CT,31088,C028\n" +
+         "ATERBURNER,WAY,31098,C050\n" +
+         "AFTON,CT,31028,C043\n" +
+         "AINSLEY,CT,31093,R004\n" +
+         "AIR PARK,DR,31088,R001.\n";
+
+      String t3 = "If random is selected then address ordering(i.e question ordering) will be random.\n" +
+      "NOTE: If more then one value in the listview is selected (say 'A' and 'B') and sequential " + 
+      "is chosen, \nthen you will have to answear all 'A' address questions before you will " + 
+      "encounter any 'B' address questions.\n\n" + 
+      "RECOMMENDATION: Only select sequential if you chose only one item in the listview.";
+
+      
+      Text text1 = new Text(t1);
+      Text text2 = new Text(t2);
+      Text text3 = new Text(t3);
+
+      
+      text1.setFont(Font.font("Times", FontWeight.BOLD, 15));
+      text2.setFont(Font.font("Times", FontWeight.BOLD, 15));
+      text3.setFont(Font.font("Times", FontWeight.BOLD, 15));
+
+      BorderPane pane = new BorderPane();
+      pane.setTop(text1);
+      pane.setCenter(text2);
+      pane.setBottom(text3);
+
+      BorderPane.setAlignment(text1, Pos.CENTER);
+      BorderPane.setAlignment(text3, Pos.BOTTOM_CENTER);
+
+      Scene scene = new Scene(pane);
+      Stage window = new Stage();
+      window.setTitle("Radio Buttons");
+      window.setScene(scene);
+      window.show();
     }
 
     
